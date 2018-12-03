98324d1feecdd55c473d989e04cb1c8fa582fa3a
Belmar <ibelmara@cuprum.cl>
2018-12-03 17:00:57 -0300

    se carga plantilla y maqueta login

---
+++ my_store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static 
+from django.conf import settings 
+from django.conf.urls import url, include
+from django.conf import settings 
+
+
+
+urlpatterns = [
+    path('', views.index, name="index")
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
